5e2c53b4f66fc773f754881579a80e7e69eedcb3
Arun Kumar M <nagarajanngl@mrac-8931.in.megatrends.com>
2013-02-11 19:05:19 +0530

    Initial Commit

---
+++ TableViewSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewSample
+//
+//  Created by Nagarajan MuthuRaj on 11/02/13.
+//  Copyright (c) 2013 AMI India. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewSample
+//
+//  Created by Nagarajan MuthuRaj on 11/02/13.
+//  Copyright (c) 2013 AMI India. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
